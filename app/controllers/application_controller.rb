8672475650a2a53a5f1dfc8b46f6dc2e0011e9b3
Martin Cabral <cabral.martin@gmail.com>
2012-09-24 13:04:47 -0300

    updated js to work with comunas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    ParcelasData.where("user_name = '#{user_name}' AND password = '#{password}'").first
   end
 
-  def parcelas_by_seccion
-    ParcelasData.where("user_name = '#{user_name}' AND password = '#{password}'").first
-  end
 end
