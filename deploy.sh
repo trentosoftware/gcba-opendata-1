7274a347a06cf9247efa3ce20ccda58f712f35ca
admin <admin@trentolabs.com.ar>
2012-11-11 03:49:17 -0300

    adding db management to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+sudo service nginx stop
 rake assets:clean
 git pull
-rake db:migrate
+rake db:drop RAILS_ENV=production
+rake db:create RAILS_ENV=production
+rake db:migrate RAILS_ENV=production
+rake import_prod:data
 rake assets:precompile
-sudo service nginx restart
+sudo service nginx start
