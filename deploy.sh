5b3d312f6e1f2d63d744442d0a4fd5434723d1ee
admin <admin@trentolabs.com.ar>
2012-11-11 17:31:47 -0300

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ git pull
 rake db:drop RAILS_ENV=production
 rake db:create RAILS_ENV=production
 rake db:migrate RAILS_ENV=production
-rake import_prod:data
+rake import:data RAILS_ENV=production
 rake assets:precompile
 sudo service nginx start
