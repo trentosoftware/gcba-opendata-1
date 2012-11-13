20968ec16cfc2cd4e268354257d73fa664a94446
Santiago Vazquez <santiagovazq@gmail.com>
2012-11-13 19:43:11 -0300

    Se agregan parametros para el map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,46 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
+    @lat = params[:lat]
+    @long = params[:long]
+    @cat = params[:category]
   end
 
   def landing
     render :layout => 'landing'
-  end
-
-  def nearest_manzanas
-    category = params[:category]
-    cat = params[:category].upcase
-    limit = params[:limit].to_i
-
-    raise 'search category must have at least 3 characters' if cat.size < 3
-    raise 'search limit must be under 200' if limit > 200
-
-    condition = "tipo2 like ?", "%#{cat}%"
-
-    lat = params[:lat].to_f
-    long = params[:long].to_f
-    search_criteria = params[:category]
-
-    #ManzanasGeometry.find()
-
-    res = ParcelasGeometry.find_by_sql ['select * ' +
-                                      'from manzanas_geometries as parm inner join parcelas_data as pard ' +
-                                            'on pard.manzana = parm.manz' +
-                                      ' where pard.tipo2 like ? or pard.nombre like ? ' +
-                                            'order by ST_Transform(parm.geometry, 4326) <-> ST_Point(?,?) ' +
-                                            ' limit ?',
-                                        "%#{cat}%", "%#{cat}%", long, lat, 100]
-
-    response = add_geo_json_header(res)
-
-    response['features'].each do |elem|
-      elem['properties']['parcelas_data'].reject! do |el|
-        el['tipo2'].upcase.index(cat).nil? and el['nombre'].upcase.index(cat).nil?
-      end
-    end
-
-    render :json => response
-
   end
 
   def parcelas_by_tag
