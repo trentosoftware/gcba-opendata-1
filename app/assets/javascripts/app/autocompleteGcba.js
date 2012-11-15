7ea1da6f772b2d4bf2f7426ad483a1e33f58a75c
Santiago Vazquez <santiagovazq@gmail.com>
2012-11-14 22:07:18 -0300

    Se agrega setter de selector en clase de autocomplete

--- app/assets/javascripts/app/autocompleteGcba.js
+++ app/assets/javascripts/app/autocompleteGcba.js
@@ -1,6 +1,6 @@
 var AutocompleteGcba = function(){
 
-    var direccion, createAutocomplete, setCallback, callback, onReadyCallback, setAfterRenderCallback;
+    var direccion, createAutocomplete, setCallback, callback, onReadyCallback, setAfterRenderCallback, selector;
 
     setCallback = function(cb) {
         callback = cb;
@@ -10,17 +10,23 @@ var AutocompleteGcba = function(){
         onReadyCallback = cb;
     }
 
+    setSelector = function(sel) {
+        selector = sel;
+    }
+
     createAutocomplete = function(selector) {
         new usig.AutoCompleter(selector, {
             rootUrl: 'http://servicios.usig.buenosaires.gob.ar/usig-js/2.3/',
             skin: 'usig4',
             onReady: function() {
                 onReadyCallback();
+                $("#" + selector).trigger('keypress');
             },
             afterSelection: function(option) {
                 direccion = option;
             },
             afterGeoCoding: function(pt) {
+                alert('ads');
                 if (pt instanceof usig.Punto) {
                     if (direccion instanceof usig.Direccion) {
                         direccion.setCoordenadas(pt);
@@ -36,6 +42,7 @@ var AutocompleteGcba = function(){
 
     return {
         init : function(selector, afterGeocodingCallback, afterRenderCallback){
+            setSelector(selector);
             setCallback(afterGeocodingCallback);
             setAfterRenderCallback(afterRenderCallback);
             createAutocomplete(selector);
